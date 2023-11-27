9b8914490e49a10a4fed5bf56715e6fa1586208a
Jiuk Choi <jiukchoi@algotradingsysteminc.com>
2023-11-27 12:14:24 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var figlet = require("figlet");
 
-figlet("Hello World!!", function (err, data) {
+figlet("Hi, there", function (err, data) {
   if (err) {
     console.log("Something went wrong...");
     console.dir(err);
