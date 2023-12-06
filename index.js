8ed18ea7167d3b73cbc58a02eb9c221076c0e3e6
Jiuk Choi <jiukchoi@cjuartintelligence.com>
2023-12-06 16:27:03 -0600

    update index.js

--- index.js
+++ index.js
@@ -1,26 +1,36 @@
 const express = require('express')
 const app = express()
+var cors = require('cors')
 const port = 3000
+
+app.use(cors())
 
 app.get('/', (req, res)=> {
   res.send('Hello World')
 })
 
 app.get('/dog', (req, res)=> {
-  res.send('dog')
-})
-
-app.get('/user/:id',(req,res)=>{
-  const p = req.params
-  console.log(p)
-  const q = req.query
-  console.log(p)  
-  res.json({'userID':p.id})
+  res.json({'sound':'dog'})
 })
 
 app.get('/cat', (req, res)=> {
-  res.send('cat')
+  res.json({'sound':'cat'})
 })
+
+app.get('/sound/:name', (req, res)=>{
+  const { name } = req.params
+  if (name == 'dog'){
+    res.json({'sound': name})
+  }else if(name == 'cat'){
+    res.json(({'sound': name}))
+  }else{
+    res.json({ 'sound':'unknown'})
+  }
+  console.log(name)
+  res.json({ name })
+})
+
+
 
 app.listen(port,()=>{
   console.log(`listening on port ${port}`)
